e4f62219dba9a1cf457a3f4603ff66937777943d
ThangQ <buithang.261001@gmail.com>
2021-11-01 09:46:12 +0700

    add comment

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 echo "HAHA";
 echo "HOHO";
 echo "HIHI";
+/**
+ * TODO: git basic command
+ * *git init
+ * *git status
+ * *git add
+ * *git commit -a [-m "<name>"]
+ * TODO: Linking an existing project to a Git Remote
+ * *1: git init
+ * *2: git add *
+ * *3: git commit -a -m "<commit name>"
+ * *4: git remote  add [origin] <git repo url>
+ */
